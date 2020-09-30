4580ed23a96981a7d8d8e8f732ca30985bf37971
Maksym Knyzhenko <knighenko@gmail.com>
2020-09-30 19:54:01 +0200

    Implemented function which can add information in database about favourite search

--- app/src/main/java/com/knighenko/realfree/activities/FavouriteSearch.java
+++ app/src/main/java/com/knighenko/realfree/activities/FavouriteSearch.java
@@ -15,8 +15,9 @@ import android.widget.CompoundButton;
 
 import com.knighenko.realfree.R;
 import com.knighenko.realfree.entity.Advertisement;
+import com.knighenko.realfree.model.UrlOfPages;
 
-public class FavouriteSearch extends AppCompatActivity  {
+public class FavouriteSearch extends AppCompatActivity {
     private Toolbar toolbar;
     private SQLiteDatabase myDB;
     private SwitchCompat switchCompat;
@@ -33,11 +34,18 @@ public class FavouriteSearch extends AppCompatActivity  {
         switchCompat = (SwitchCompat) findViewById(R.id.switch_home_garden);
         switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                System.out.println("Switch State="+isChecked);
+                if (isChecked == true) {
+                    System.out.println("Switch State=" + isChecked);
+                    createDB();
+                    addToDB(UrlOfPages.HOME_GARDEN.getTitle(), UrlOfPages.HOME_GARDEN.getUrl(), myDB);
+                }
+                if (isChecked==false){
+
+                }
+                System.out.println("Is in database: " + checkInDB(UrlOfPages.HOME_GARDEN.getTitle()));
             }
         });
     }
-
 
 
     @Override
@@ -49,32 +57,40 @@ public class FavouriteSearch extends AppCompatActivity  {
     }
 
 
-
     /**
      * Метод создает базу данных или открывает созданную + создает таблицу advertisement
      */
     private void createDB() {
         myDB = openOrCreateDatabase("my.db", MODE_PRIVATE, null);
-        myDB.execSQL("CREATE TABLE IF NOT EXISTS advertisement ( title TEXT, url TEXT, srcUrl Text)");
+        myDB.execSQL("CREATE TABLE IF NOT EXISTS favourite_search ( title TEXT, url TEXT)");
 
     }
 
     /**
      * Метод добавляет элементы в базу данных
      */
-    private void addToDB(Advertisement adv, SQLiteDatabase myDB) {
+    private void addToDB(String title, String url, SQLiteDatabase myDB) {
         ContentValues row = new ContentValues();
-        row.put("title", adv.getTitle());
-        row.put("url", adv.getUrl());
-        row.put("srcUrl", adv.getImageSrc());
-        myDB.insert("advertisement", null, row);
+        row.put("title", title);
+        row.put("url", url);
+        myDB.insert("favourite_search", null, row);
 
     }
+    /**
+     * Метод удаляет элементы из базу данных
+     */
+    private void deleteFromDB(String title,  SQLiteDatabase myDB) {
+        ContentValues row = new ContentValues();
+        row.put("title", title);
+        myDB.insert("favourite_search", null, row);
+
+    }
+
     /**
      * Метод читает из базы данных избранные рубрики для поиска
      */
     private boolean checkInDB(String title) {
-        Cursor cursor = myDB.rawQuery("SELECT EXISTS (select * from  advertisement WHERE title = \'" + title + "\'  LIMIT 1)", null);
+        Cursor cursor = myDB.rawQuery("SELECT EXISTS (select url from  favourite_search WHERE title = \'" + title + "\'  LIMIT 1)", null);
         while (cursor.moveToNext()) {
             if (cursor.getInt(0) == 1) {
                 cursor.close();
