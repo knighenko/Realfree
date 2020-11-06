f39269ae0c032da6562ebb84c538bca62cf9f05c
Maksym Knyzhenko <knighenko@gmail.com>
2020-11-06 21:27:38 +0200

    Add ZOOPRODUCT in searchAdvActivity

--- app/src/main/java/com/knighenko/realfree/activities/SearchAdvertisements.java
+++ app/src/main/java/com/knighenko/realfree/activities/SearchAdvertisements.java
@@ -98,6 +98,13 @@ public class SearchAdvertisements extends AppCompatActivity {
                 startActivity(intent);
                 Toast.makeText(this, "Поиск в рубрике " + UrlOfPages.WORLD_OF_CHILDREN.getTitle(), Toast.LENGTH_LONG).show();
                 break;
+            case R.id.world_of_children:
+
+                intent = new Intent(SearchAdvertisements.this, MainActivity.class);
+                intent.putExtra("url", UrlOfPages.WORLD_OF_CHILDREN.getUrl() + 1);
+                startActivity(intent);
+                Toast.makeText(this, "Поиск в рубрике " + UrlOfPages.WORLD_OF_CHILDREN.getTitle(), Toast.LENGTH_LONG).show();
+                break;
         }
     }
 
