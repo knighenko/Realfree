d49eb88e4f06df632fa84e152a173fd44536c8e8
Maksym Knyzhenko <knighenko@gmail.com>
2020-11-08 21:25:37 +0200

    Change radius of search

--- app/src/main/java/com/knighenko/realfree/model/UrlOfPages.java
+++ app/src/main/java/com/knighenko/realfree/model/UrlOfPages.java
@@ -1,13 +1,13 @@
 package com.knighenko.realfree.model;
 
 public enum UrlOfPages {
-    HOME_GARDEN("https://www.olx.ua/dom-i-sad/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=10", "Дом и сад"),
-    ELECTRONICS("https://www.olx.ua/elektronika/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=10", "Электроника"),
-    TRANSPORT_PARTS("https://www.olx.ua/zapchasti-dlya-transporta/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=10", "Запчасти к транспорту"),
-    BUSINESS_AND_SERVICES("https://www.olx.ua/uslugi/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=10", "Бизнес и услуги"),
-    FASHION_AND_STYLE("https://www.olx.ua/moda-i-stil/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=10", "Мода и стиль"),
-    HOBBIES_AND_LEISURE("https://www.olx.ua/hobbi-otdyh-i-sport/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=10", "Хобби и досуг"),
-    WORLD_OF_CHILDREN("https://www.olx.ua/detskiy-mir/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=10", "Детский мир"),
+    HOME_GARDEN("https://www.olx.ua/dom-i-sad/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Дом и сад"),
+    ELECTRONICS("https://www.olx.ua/elektronika/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Электроника"),
+    TRANSPORT_PARTS("https://www.olx.ua/zapchasti-dlya-transporta/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Запчасти к транспорту"),
+    BUSINESS_AND_SERVICES("https://www.olx.ua/uslugi/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Бизнес и услуги"),
+    FASHION_AND_STYLE("https://www.olx.ua/moda-i-stil/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Мода и стиль"),
+    HOBBIES_AND_LEISURE("https://www.olx.ua/hobbi-otdyh-i-sport/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Хобби и досуг"),
+    WORLD_OF_CHILDREN("https://www.olx.ua/detskiy-mir/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Детский мир"),
     ZOOPRODUCT("https://www.olx.ua/uk/zhivotnye/tovary-dlya-zhivotnyh/kha/?search%5Bfilter_float_price%3Afrom%5D=free", "Зоотовары");
 
     private String url;
