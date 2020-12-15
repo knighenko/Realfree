6521a7e20a274a3e0159ee98c940a8dbfed0093c
Maksym Knyzhenko <knighenko@gmail.com>
2020-12-15 20:39:26 +0200

    Add ENUM's

--- app/src/main/java/com/knighenko/realfree/model/UrlOfPages.java
+++ app/src/main/java/com/knighenko/realfree/model/UrlOfPages.java
@@ -9,7 +9,9 @@ public enum UrlOfPages {
     HOBBIES_AND_LEISURE("https://www.olx.ua/hobbi-otdyh-i-sport/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Хобби и досуг"),
     WORLD_OF_CHILDREN("https://www.olx.ua/detskiy-mir/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15", "Детский мир"),
     ZOOPRODUCT("https://www.olx.ua/uk/zhivotnye/tovary-dlya-zhivotnyh/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=50", "Зоотовары"),
-    ARISTON("https://www.olx.ua/kha/q-ariston-%D0%B1%D0%BE%D0%B9%D0%BB%D0%B5%D1%80/?search%5Bfilter_float_price%3Ato%5D=1000", "Аристон");
+    ARISTON("https://www.olx.ua/kha/q-ariston-%D0%B1%D0%BE%D0%B9%D0%BB%D0%B5%D1%80/?search%5Bfilter_float_price%3Ato%5D=1000", "Аристон"),
+    BICYCLE("https://www.olx.ua/kha/q-%D0%B2%D0%B5%D0%BB%D0%BE%D1%81%D0%B8%D0%BF%D0%B5%D0%B4/", "Велосипед"),
+    GAS_STOVE("https://www.olx.ua/kha/q-%D0%B3%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D1%8F-%D0%BF%D0%BB%D0%B8%D1%82%D0%B0/", "Газовая плита");
 
     private String url;
     private String title;
